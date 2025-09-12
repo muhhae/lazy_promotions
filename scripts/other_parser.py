33d42ba5745b590880d8771930ba1376663b9b5d
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2025-09-12 12:42:21 +0700

    add other parser

--- scripts/other_parser.py
+++ scripts/other_parser.py
@@ -1,25 +1,31 @@
 from glob import glob
-from pprint import pprint
 import re
+import os
 import pandas as pd
+
+pattern = re.compile(
+    r".*?\s+(?P<algo>[A-Za-z0-9_]+)-(?P<age>[\d\.]+)\s+cache size\s+(?P<cache_size>\d+),\s+"
+    r"(?P<requests>\d+)\s+req,\s+miss ratio\s+(?P<miss_ratio>[\d\.]+),\s+"
+    r"throughput\s+(?P<throughput>[\d\.]+)\s+MQPS,\s+promotion\s+(?P<promotion>\d+)"
+)
 
 
 def parse_line(line: str, filename: str, cache_size: float):
-    pattern = re.compile(
-        r"AGE-(?P<age>[\d\.]+)\s+cache size\s+(?P<cache_size>\d+),\s+"
-        r"(?P<requests>\d+)\s+req,\s+miss ratio\s+(?P<miss_ratio>[\d\.]+),\s+"
-        r"byte miss ratio\s+(?P<byte_miss_ratio>[\d\.]+)\s+(?P<reinserted>\d+)"
-    )
+    algorithms = {
+        "lpFIFO_batch": "Batch",
+        "LRU_delay": "Delay",
+        "lpLRU_prob": "Prob",
+    }
     match = pattern.search(line)
     if match:
         d = match.groupdict()
         return {
-            "Algorithm": "AGE",
+            "Algorithm": algorithms[d["algo"]],
             "Scale": float(d["age"]),
             "Real Cache Size": int(d["cache_size"]),
             "Request": int(d["requests"]),
             "Miss Ratio": float(d["miss_ratio"]),
-            "Reinserted": int(d["reinserted"]),
+            "Reinserted": int(d["promotion"]),
             "Trace": filename[filename.rfind("/") + 1 :],
             "Trace Path": filename,
             "Cache Size": 0.01,
@@ -42,15 +48,15 @@ def ReadData():
     datasets = {
         p[p.rfind("/") + 1 :]: re.sub(r"\.oracleGeneral\S*", "", p) for p in paths
     }
-    print(len(datasets))
     rows = []
-    outputs = sorted(glob("../age_results/*"))
+    outputs = sorted(glob("../new_results/**/*"))
     for file in outputs:
+        if os.path.isdir(file):
+            continue
         with open(file, "r") as f:
-            for i, line in enumerate(f):
-                if i % 3 != 1:
-                    continue
-                key = file[file.rfind("/") + 1 :]
+            filename = file[: file.find(".cachesim")]
+            for _, line in enumerate(f):
+                key = filename[filename.rfind("/") + 1 :]
                 if key not in datasets:
                     print(key, "doesn't exist!")
                     continue
